dcde8c3c3c3ebc8fa640500bedd0e46f4da05142
fdmx <fdmxfarhan@yahoo.com>
2020-04-28 19:38:46 +0430

    km

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ var options = {
     'phone': '09382198592',
     'mail': 'my@site.com',
     'desc': 'توضیحات پرداخت کننده',
-    'callback': 'http://localhost:3000',
+    'callback': 'http://iranroboticacademy.com',
     'reseller': null,
   },
   json: true,
